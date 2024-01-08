b2f400ab633b1955641048dbb433e75dcd0761b9
leeroy-code <lvrvfv@gmail.com>
2024-01-08 12:49:27 +0800

    项目整合完成

---
+++ cloud-gateway/src/main/java/com/xiaoli/gateway/HelloGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.xiaoli.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGatewayApplication.class,args);
+    }
+}
